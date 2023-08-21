ac7fad2c65dd04713a1d10663fdd638f5a727489
Porsudar Vallal <porsudar02@gmail.com>
2023-08-21 16:10:00 +0530

    this is my first commit

---
+++ 2D-array-creation.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int i,j,a[2][2];
+    for(i=0;i<2;i++){
+        for(j=0;j<2;j++){
+            scanf("%d",&a[i][j]);
+        }
+    }
+    for(i=0;i<2;i++){
+        for(j=0;j<2;j++){
+            printf("%d\t",a[i][j]);
+        }
+        printf("\n");
+    }
+}
